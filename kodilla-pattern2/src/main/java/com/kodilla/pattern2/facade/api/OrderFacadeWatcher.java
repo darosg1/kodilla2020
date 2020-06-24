8cd03cb31980aa724ff2b517966037356d3773ff
darosg1 <darosg1@yahoo.com>
2020-06-24 15:22:03 +0200

    Zadanie 24.3 Logowanie żądań do fasady

---
+++ kodilla-pattern2/src/main/java/com/kodilla/pattern2/facade/api/OrderFacadeWatcher.java
@@ -0,0 +1,25 @@
+package com.kodilla.pattern2.facade.api;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Aspect
+@Component
+public class OrderFacadeWatcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacadeWatcher.class);
+
+    @Before("execution(* com.kodilla.pattern2.facade.api.OrderFacade.processOrder(..))"+"&& target(object) && args(order, userId)")
+            public void logEvent(OrderDto order, Long userId, Object object) {
+        LOGGER.info("Class: " + object.getClass().getName() + " Args items: " +
+                order.getItems().toString()
+                /*order.getItems().stream()
+                        .map(o -> o.getProductId().toString())
+                        .collect(Collectors.joining(", "))*/
+                + ", userId" + userId.toString());
+    }
+}
